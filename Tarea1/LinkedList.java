1eeef0789b9604c39e25ede3dbbff9a4fb3c907b
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-08-30 13:55:36 +0300

    Add files via upload

---
+++ Tarea1/LinkedList.java
@@ -0,0 +1,214 @@
+import java.util.*;
+
+public class LinkedList {
+    public Node head;
+    public Node tail;
+
+    public LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public void addInTail(Node item) {
+        if (this.head == null)
+            this.head = item;
+        else
+            this.tail.next = item;
+        this.tail = item;
+    }
+
+    public Node find(int value) {
+        Node node = this.head;
+        while (node != null) {
+            if (node.value == value)
+                return node;
+            node = node.next;
+        }
+        return null;
+    }
+
+    public ArrayList<Node> findAll(int _value) {
+        ArrayList<Node> nodes = new ArrayList<Node>();
+
+        Node cur_node = head;
+        while (cur_node != null) {
+            if (cur_node.value == _value) {
+                Node f_node = cur_node;
+                nodes.add(f_node);
+            }
+            cur_node = cur_node.next;
+        }
+        return nodes;
+    }
+
+    public boolean remove(int _value) {
+
+        if (count() == 1 && head.value == _value) {
+            clear();
+            return true;
+        }
+
+        Node prev_node = null;
+        Node cur_node = head;
+
+        while (cur_node != null) {
+            if (cur_node.value == _value & prev_node != null) {
+                prev_node.next = cur_node.next;
+
+                if (cur_node.next == null) {
+                    tail = prev_node;
+                }
+                cur_node = null;
+                return true;        // deleted
+            }
+
+            if (cur_node.value == _value) {
+                head = cur_node.next;
+                cur_node = null;
+                return true;
+            }
+            prev_node = cur_node;
+            cur_node = prev_node.next;
+        }
+        return false;
+    }
+
+
+    public void removeAll(int _value) {
+        Node prev_node = null;
+        Node cur_node = head;
+
+        while (cur_node != null) {
+
+            if (cur_node.value == _value & prev_node != null) {
+                prev_node.next = cur_node.next;
+
+                if (cur_node.next == null) {
+                    tail = prev_node;
+                }
+                cur_node = null;
+                cur_node = prev_node.next;
+                continue;
+            }
+
+            if (cur_node.value == _value && cur_node == tail){  //prev_node == null
+                clear();
+                break;
+            }
+
+            if (cur_node.value == _value)  {         //prev_node == null
+                head = cur_node.next;
+                cur_node = null;
+                cur_node = head;
+                continue;
+            }
+
+            prev_node = cur_node;
+            cur_node = prev_node.next;
+        }
+    }
+
+
+    public void clear() {
+        Node next_node = head.next;
+
+        while (head.next != null) {
+            next_node = head.next;
+            head = next_node;
+        }
+        head = null;
+        tail = null;
+    }
+
+
+    public int count() {
+        Node cur_node = head;
+        int ret = 0;
+        while (cur_node != null) {
+            ret++;
+            cur_node = cur_node.next;
+        }
+
+        return ret;
+    }
+
+
+    public void insertAfter(Node _nodeAfter, Node _nodeToInsert) {
+        Node tmp = null;
+
+        if (count() == 0) {
+            tail = _nodeToInsert;
+        }
+
+        if (_nodeAfter == tail) {
+            tail = _nodeToInsert;
+        }
+
+        if (_nodeAfter == null) {
+            _nodeToInsert.next = head;
+            head = _nodeToInsert;
+            return;
+        }
+
+        Node cur_node = head;
+        while (cur_node != null) {
+            if (cur_node == _nodeAfter) {
+                tmp = cur_node.next;
+                cur_node.next = _nodeToInsert;
+                _nodeToInsert.next = tmp;
+                return;
+            }
+            cur_node = cur_node.next;
+        }
+    }
+
+
+    // my func  creating list
+    public void add_nodes() {
+        addInTail(new Node(12));
+        addInTail(new Node(18));
+        addInTail(new Node(12));
+        addInTail(new Node(15));
+        addInTail(new Node(17));
+        addInTail(new Node(12));
+    }
+
+
+    public static void main(String[] args) {
+        LinkedList s_list = new LinkedList();
+        int n = 0;
+
+        s_list.add_nodes();
+        n = s_list.count();
+        ArrayList<Node> arr = s_list.findAll(12);
+
+        s_list.removeAll(12);
+        n = s_list.count();
+
+        s_list.clear();
+        n = s_list.count();
+    }
+
+}
+
+    class Node {
+        public int value;
+        public Node next;
+
+        public Node(int _value) {
+            value = _value;
+            next = null;
+        }
+    }
+
+
+
+
+
+
+  //  public  Node sum_node (Node a1, Node a2 ) {
+ //       return null;
+ //   }
+
+
+
