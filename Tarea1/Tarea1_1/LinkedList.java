e27702bc1752a411eca7e5165a97cc4f1aa089d9
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-09-13 23:44:31 +0300

    Add files via upload

---
+++ Tarea1/Tarea1_1/LinkedList.java
@@ -0,0 +1,225 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
+public class LinkedList {
+    public Node head;
+    public Node tail;
+
+    public LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public void addInTail(Node item) {
+        if (this.head == null)
+            this.head = item;
+        else
+            this.tail.next = item;
+        this.tail = item;
+    }
+
+    public Node find(int value) {
+        Node node = this.head;
+        while (node != null) {
+            if (node.value == value)
+                return node;
+            node = node.next;
+        }
+        return null;
+    }
+
+    public ArrayList<Node> findAll(int _value) {
+        ArrayList<Node> nodes = new ArrayList<Node>();
+
+        Node cur_node = head;
+        while (cur_node != null) {
+            if (cur_node.value == _value) {
+                Node f_node = cur_node;
+                nodes.add(f_node);
+            }
+            cur_node = cur_node.next;
+        }
+        return nodes;
+    }
+
+    public boolean remove(int _value) {
+
+        if (count() == 1 && head.value == _value) {
+            clear();
+            return true;
+        }
+
+        Node prev_node = null;
+        Node cur_node = head;
+
+        while (cur_node != null) {
+            if (cur_node.value == _value & prev_node != null) {
+                prev_node.next = cur_node.next;
+
+                if (cur_node.next == null) {
+                    tail = prev_node;
+                }
+                cur_node = null;
+                return true;        // deleted
+            }
+
+            if (cur_node.value == _value) {
+                head = cur_node.next;
+                cur_node = null;
+                return true;
+            }
+            prev_node = cur_node;
+            cur_node = prev_node.next;
+        }
+        return false;
+    }
+
+
+    public void removeAll(int _value) {
+        Node prev_node = null;
+        Node cur_node = head;
+
+        while (cur_node != null) {
+
+            if (cur_node.value == _value & prev_node != null) {
+                prev_node.next = cur_node.next;
+
+                if (cur_node.next == null) {
+                    tail = prev_node;
+                }
+                cur_node = null;
+                cur_node = prev_node.next;
+                continue;
+            }
+
+            if (cur_node.value == _value && cur_node == tail){  //prev_node == null
+                clear();
+                break;
+            }
+
+            if (cur_node.value == _value)  {         //prev_node == null
+                head = cur_node.next;
+                cur_node = null;
+                cur_node = head;
+                continue;
+            }
+
+            prev_node = cur_node;
+            cur_node = prev_node.next;
+        }
+    }
+
+
+    public void clear() {
+        Node next_node = head.next;
+
+        while (head.next != null) {
+            next_node = head.next;
+            head = next_node;
+        }
+        head = null;
+        tail = null;
+    }
+
+
+    public int count() {
+        Node cur_node = head;
+        int ret = 0;
+        while (cur_node != null) {
+            ret++;
+            cur_node = cur_node.next;
+        }
+
+        return ret;
+    }
+
+
+    public void insertAfter(Node _nodeAfter, Node _nodeToInsert) {
+        Node tmp = null;
+
+        if (count() == 0) {
+            head = _nodeToInsert;
+            tail = _nodeToInsert;
+            return;
+        }
+
+        if (_nodeAfter == tail) {
+            tail = _nodeToInsert;
+        }
+
+        if (_nodeAfter == null) {
+            _nodeToInsert.next = head;
+            head = _nodeToInsert;
+            return;
+        }
+
+        Node cur_node = head;
+        while (cur_node != null) {
+            if (cur_node == _nodeAfter) {
+                tmp = cur_node.next;
+                cur_node.next = _nodeToInsert;
+                _nodeToInsert.next = tmp;
+                return;
+            }
+            cur_node = cur_node.next;
+        }
+    }
+
+
+    public LinkedList SumList (LinkedList L1, LinkedList L2) {
+        if (L1.count() != L2.count()) return null;
+
+        Node n1 = L1.head;
+        Node n2 = L2.head;
+        int vav1, val2;
+        LinkedList  Ls = new LinkedList();
+      //  Node ns = new Node(0);
+
+        for (int i = 0; i < L1.count(); i++ ) {
+            //if (i == 0) {
+             //   Ls.head = ns;
+              //  Ls.tail = ns;
+            //}
+            Node ns = new Node( n1.value + n2.value);
+           // ns.value = n1.value + n2.value;
+            Ls.addInTail(ns);
+
+            n1 = n1.next;
+            n2 = n2.next;
+            //ns = ns.next;
+
+        }
+
+        return Ls;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+    }
+/*
+ * 8. Напишите функцию, которая получает на вход два связных списка, состоящие из целых значений,
+ *  и если их длины равны, возвращает список, каждый элемент которого равен сумме
+ * соответствующих элементов входных списков.
+ */
+
+}
+
+
+class Node {
+    public int value;
+    public Node next;
+
+    public Node(int _value) {
+        value = _value;
+        next = null;
+    }
+}
