e4a6df5f5139ac7c6c71d04ae18b0e1a85f25ccb
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-09-26 16:36:15 +0300

    Add files via upload

--- Tarea3/DynArray.java
+++ Tarea3/DynArray.java
@@ -1,3 +1,5 @@
+//import java.lang.reflect.Array;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class DynArray<T>
@@ -10,50 +12,16 @@ public class DynArray<T>
     public DynArray(Class clz)
     {
         clazz = clz;
-        // new DynArray<Integer>(Integer.class);
         count = 0;
         makeArray(16);
     }
-   
+
     public void makeArray (int new_capacity)// new capacity - занять память
-    {   //array = (T[]) Array.newInstance(this.clazz, new_capacity);
-        int req_capacity = this.capacity;
-
-        if (this.capacity == 0) {
-            this.capacity = 16;
-            this.array = (T[]) new Integer [16];
-            return;
-        }
-
-        boolean f = false;
-
-        if (new_capacity < 16) {
-            req_capacity = 16;
-            f = true;
-        }
-
-        int old_capacity = this.capacity;
-        // Low
-        while (new_capacity < this.capacity & f ==  false) {
-            req_capacity = req_capacity * 2 / 3;
-            if (new_capacity >= req_capacity) {
-                req_capacity = old_capacity;
-                f = true;
-                break;
-            }
-            old_capacity  = req_capacity;
-        }
-        // Up
-        while (new_capacity >  req_capacity & f ==  false) {
-                req_capacity *= 2;  //
-        }
-
-        new_capacity = req_capacity;
-        Integer [] myarr = new Integer [new_capacity];
-
-        System.arraycopy(this.array, 0,  myarr, 0, this.count);
+    {
+        this.array = (T[]) Array.newInstance(this.clazz, new_capacity);
         this.capacity = new_capacity;
-        this.array = (T[]) myarr;
+     //  this.array = myarr;
+        // переписывание массива тут же
     }
 
 
@@ -68,53 +36,74 @@ public class DynArray<T>
 
     public void append(T itm)
     {
-      int n = this.count;
+        if (this.capacity < this.count + 1) {
+            T[] myarr = this.array;
+            makeArray(this.capacity * 2);
 
-      if (this.capacity <= count) {
-          makeArray(count + 1);
-      }
-        Integer itm1 = (Integer) itm;
-        this.array[n] = (T) itm1;
-        count++;
+            for (int i = 0; i < myarr.length; i++) {
+                this.array[i] = myarr[i];
+            }
+        }
+
+        this.array[this.count] = (T) itm;
+        count ++;
     }
+
 
     public void insert(T itm, int index)
     {
-      if (index >= this.count)
-          return;
-        Integer [] arrtmp = new Integer [this.count + 1];
+        if (index >= this.count || index < 0) {
+            throw new ArrayIndexOutOfBoundsException("Ошибочный индекс");
+        }
+
+        if (this.capacity < this.count + 1) {
+            T[] myarr = this.array;
+            makeArray(this.capacity * 2);
+
+            for (int i = 0; i < myarr.length; i++) {
+                this.array[i] = myarr[i];
+            }
+        }
+
+        T[] arrtmp = (T[]) Array.newInstance(this.clazz, this.count + 1);
         System.arraycopy (this.array, 0, arrtmp, 0, index);
-        arrtmp [index] = (Integer) itm;
+        arrtmp [index] =  itm;
         System.arraycopy (this.array, index, arrtmp, index+1, this.count - index);
 
-      if (arrtmp.length > this.capacity)
-        makeArray (arrtmp.length);
-
-        this.array = (T[])arrtmp;
-        this.count = this.array.length;
+        this.array = arrtmp;
+        this.count = this.count+1;
     }
 
     public void remove(int index)
     {
-        if(index >= this.count) {
-            return;
+        if (index >= this.count || index < 0) {
+            throw new ArrayIndexOutOfBoundsException("Ошибочный индекс");
         }
 
-        Integer [] arrtmp = new Integer [this.count - 1];
-        System.arraycopy (this.array, 0, arrtmp, 0, index);
+        if ( this.count - 1 < 0.5 * this.capacity) {
+            T[] myarr = this.array;
 
+           int ncapacity = this.capacity * 2/3;
+
+            if (ncapacity < 16) {
+                ncapacity = 16;
+            }
+            makeArray(ncapacity);
+
+            for (int i = 0; i < myarr.length; i++) {
+                this.array[i] = myarr[i];
+            }
+        }
+
+
+        T[] arrtmp = (T[]) Array.newInstance(this.clazz, this.count - 1);
+        System.arraycopy (this.array, 0, arrtmp, 0, index);
         System.arraycopy (this.array, index+1, arrtmp, index, this.count - index-1);
 
-        if ( arrtmp.length < this.capacity / 2)
-            makeArray (arrtmp.length);
-
-        this.array = (T[])arrtmp;
+        this.array = arrtmp;
         this.count = this.array.length;
     }
 
-
-    
-} 
-
-
-
+    public static void main(String[] args) {
+    }
+}
