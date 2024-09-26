08eaf7c9ddb0631391ecd867d350a9ce21997ad4
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-09-26 16:40:47 +0300

    Add files via upload

--- Tarea3/DynArrayTest.java
+++ Tarea3/DynArrayTest.java
@@ -4,41 +4,31 @@ import static org.junit.Assert.assertTrue;
 
 public class DynArrayTest {
     @Test
-    public void test0() {
-        DynArray d1 = new DynArray(Integer.class);
-        d1.makeArray(100);
-        assertTrue(d1.capacity == 128);
-
-        d1.makeArray(50);
-        assertTrue(d1.capacity == 56);
+    public void test0() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
 
         DynArray d2 = new DynArray<Integer>(Integer.class);
-        assertTrue(d2.capacity == 16);
-        d2.makeArray(500);
-        d2.makeArray(0);
-        assertTrue(d2.capacity == 16);
-        d2.makeArray(0);
 
-        d2.makeArray(17);
-        assertTrue(d2.capacity == 32);
-        d2.makeArray(25);
-        assertTrue(d2.capacity == 32);
-        d2.makeArray(200);
-        assertTrue(d2.capacity == 256);
-        d2.makeArray(5);
-        assertTrue(d2.capacity == 16);
+     //   Во время выполнения вы можете создать экземпляр Integer,
+     //   экземпляр использует объектно-подобную компоновку памяти, например, с указателем
+     //   в начале, указывающим на Integer.class,
+     //   что обеспечивает полиморфное поведение java во время выполнения.
 
-        int a = 1;
         for (int i = 0; i < 16; i++) {
-            d2.append(i);
+            d2.append (i);
         }
 
         assertTrue(d2.capacity == 16);
         assertTrue(d2.count == 16);
 
-        d2.insert(100, 15);
+        Integer k = 100;          //array = (T[]) Array.newInstance(this.clazz, new_capacity);
+        d2.insert(k , 15);
         assertTrue(d2.capacity == 32);
         assertTrue(d2.count == 17);
+
+       // d2.insert(k , -2);
+        // d2.insert(k , 150);
+        //assertTrue(d2.capacity == 32);
+        //assertTrue(d2.count == 17);
 
         Integer res = (Integer) d2.getItem(15);
         assertTrue(res == 100);
@@ -52,7 +42,8 @@ public class DynArrayTest {
 
 
         for (int i = 10; i < 30; i++) {
-            d2.insert(i, i);
+
+            d2.insert(i+1, i);
         }
 
         assertTrue(d2.capacity == 64);
@@ -72,6 +63,5 @@ public class DynArrayTest {
         d2.remove(0);
         assertTrue(d2.count == 30);
         assertTrue(d2.capacity == 42);
-
     }
 }
