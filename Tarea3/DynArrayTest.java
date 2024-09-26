1750331e1cbec2841fb7cdb3e41aa805ec713f83
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-09-26 22:24:48 +0300

    Add files via upload

--- Tarea3/DynArrayTest.java
+++ Tarea3/DynArrayTest.java
@@ -13,22 +13,35 @@ public class DynArrayTest {
      //   в начале, указывающим на Integer.class,
      //   что обеспечивает полиморфное поведение java во время выполнения.
 
+        d2.insert(4,0);
+
         for (int i = 0; i < 16; i++) {
             d2.append (i);
         }
 
-        assertTrue(d2.capacity == 16);
+        assertTrue(d2.capacity == 32);
+        assertTrue(d2.count == 17);
+
+        d2.remove(0);
+        assertTrue(d2.capacity == 32);
+        assertTrue(d2.count == 16);
+        d2.insert(7,0);
+        assertTrue(d2.capacity == 32);
+        assertTrue(d2.count == 17);
+        d2.remove(d2.count-1);
+        assertTrue(d2.capacity == 32);
+        assertTrue(d2.count == 16);
+        d2.insert(55,d2.count);
+        assertTrue(d2.capacity == 32);
+        assertTrue(d2.count == 17);
+        d2.remove(d2.count-2);
+        assertTrue(d2.capacity == 32);
         assertTrue(d2.count == 16);
 
         Integer k = 100;          //array = (T[]) Array.newInstance(this.clazz, new_capacity);
         d2.insert(k , 15);
         assertTrue(d2.capacity == 32);
         assertTrue(d2.count == 17);
-
-       // d2.insert(k , -2);
-        // d2.insert(k , 150);
-        //assertTrue(d2.capacity == 32);
-        //assertTrue(d2.count == 17);
 
         Integer res = (Integer) d2.getItem(15);
         assertTrue(res == 100);
@@ -40,9 +53,7 @@ public class DynArrayTest {
         assertTrue(d2.capacity == 32);
         assertTrue(d2.count == 16);
 
-
         for (int i = 10; i < 30; i++) {
-
             d2.insert(i+1, i);
         }
 
@@ -63,5 +74,10 @@ public class DynArrayTest {
         d2.remove(0);
         assertTrue(d2.count == 30);
         assertTrue(d2.capacity == 42);
+
+        // d2.insert(k , -2);
+        // d2.insert(k , 150);
+        //assertTrue(d2.capacity == 32);
+        //assertTrue(d2.count == 17);
     }
 }
