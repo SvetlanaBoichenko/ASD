34059bb4f410591331c7f1e37fca838a62648d44
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-10-02 16:15:37 +0300

    Update StackTest.java

--- Tarea4/StackTest.java
+++ Tarea4/StackTest.java
@@ -25,14 +25,28 @@ public class StackTest {
         st.pop();
         s = st.size();
         assertTrue(s == 2);
+        y = (Node)st.peek();
 
 
         y= (Node) st.pop();
         assertTrue( (Integer)y.value == 2);
+        s = st.size();
+        assertTrue(s == 1);
+        y = (Node)st.peek();
+        assertTrue( (Integer)y.value == 1);
+
+
         Node r1 =  (Node) st.pop();
         assertTrue((Integer)r1.value == 1);
+        s = st.size();
+        assertTrue(s == 0);
+        y = (Node)st.peek();
+
+
         r1 =  (Node) st.pop();
         assertTrue(r1 == null);
+        y = (Node)st.peek();
+
 
         y =  (Node) st.pop();
         assertTrue(y == null);
