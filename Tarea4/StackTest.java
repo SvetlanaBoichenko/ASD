3a10388acd19554e95430cc30d0839088e2aed5c
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-10-03 16:08:38 +0300

    Update StackTest.java

--- Tarea4/StackTest.java
+++ Tarea4/StackTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 public class StackTest {
     @Test
     public void test0() {
-        Stack st = new Stack();
+        Stack<Integer> st = new Stack<Integer>();
         st.push(1);
 
         int s = st.size();
@@ -19,39 +19,56 @@ public class StackTest {
         s = st.size();
         assertTrue(s == 3);
 
-       Integer y = (Integer)st.peek();
+       Integer y = st.peek();
         assertTrue(y == 3);
 
      //  Node a = (Node) st.pop();
-        y = (Integer) st.pop();
+        y =  st.pop();
         assertTrue(y == 3);
         s = st.size();
         assertTrue(s == 2);
-        y = (Integer)st.peek();
+        y = st.peek();
         assertTrue(y == 2);
 
-        y = (Integer) st.pop();
+        y =  st.pop();
         assertTrue( y == 2);
         s = st.size();
         assertTrue(s == 1);
-        y = (Integer)st.peek();
+        y = st.peek();
         assertTrue( y == 1);
 
 
-        y = (Integer) st.pop();
+        y =  st.pop();
         assertTrue(y == 1);
         s = st.size();
         assertTrue(s == 0);
-        y = (Integer)st.peek();
+        y = st.peek();
         assertTrue(y == null);
 
-        y = (Integer) st.pop();
+        y =  st.pop();
         assertTrue(y == null);
-        y = (Integer)st.peek();
+        y = st.peek();
         assertTrue(y == null);
 
         s = st.size();
         assertTrue(s == 0);
+
+        st.push(3);
+       y= st.pop();
+
+
+        Stack<String> st1 = new Stack<String>();
+        st1.push("1");
+
+        assertTrue(y == null);
+        st1.push("2");
+        st1.push("3");
+        st1.push("4");
+
+        String h1 =  st1.pop();
+        h1 =  st1.pop();
+        h1 =  st1.pop();
+
 
     }
 }
