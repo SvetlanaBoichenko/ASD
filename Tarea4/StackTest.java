45eb15ea99aab04a0ec442b93acc32162b7cf0a7
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-10-04 08:44:53 +0300

    Update StackTest.java

--- Tarea4/StackTest.java
+++ Tarea4/StackTest.java
@@ -4,60 +4,74 @@ import static org.junit.Assert.*;
 
 public class StackTest {
     @Test
-    public void test0() {
-        Stack<Integer> st = new  Stack<>();
+    public void test_push_peek() {
+        Stack<Integer> st = new Stack<>();
         int s = 0;
         int N = 10;
 
         Integer pik = 0;
-        for (int i = 0; i < N; i ++) {
+        for (int i = 0; i < N; i++) {
             st.push(i);
             s = st.size();
             assertEquals(s, i + 1);
             pik = st.peek();
             assertEquals(pik.intValue(), i);
         }
+    }
+    @Test
+    public void test_pop() {
+        Stack<Integer> st1 = new Stack<>();
+        int s1 = 0;
+        int N1 = 10;
+        Integer p1, pik1;
 
-        Integer p = 0;
-        for (int i = N-1; i >= 0; i --) {
-            p = st.pop();
-            assertEquals(p.intValue(), i);
-            s = st.size();
-            assertEquals(s, i);
 
-            if (i > 0) {
-                pik = st.peek();
-                assertEquals(pik.intValue(), i - 1);
-            }
-            else {
-                pik = st.peek();
-                assertNull(pik);
-            }
-        }
+        s1= st1.size();
+        assertEquals(0, s1);
 
-         s = st.size();
-        assertEquals(0, s);
-
-        st.push(2);
-        st.push(3);
-        st.push(4);
-        st.push(5);
-        st.push(6);
+        st1.push(2);
+        st1.push(3);
+        st1.push(4);
+        st1.push(5);
+        st1.push(6);
 
         for (int i = 6; i >= 4; i --) {
-            p = st.pop();
-            assertEquals(p.intValue(), i);
+            p1 = st1.pop();
+            assertEquals(p1.intValue(), i);
+            pik1 = st1.peek();
+            assertEquals(pik1.intValue(), i - 1);
         }
+        s1= st1.size();
+        assertEquals(2, s1);
+        pik1 = st1.peek();
+        assertEquals(pik1.intValue(), 3);
 
-        st.push (7);
-        p = st.pop();
-        assertEquals(7, (int) p);
-        p = st.pop();
-        assertEquals(3, (int) p);
-        p = st.pop();
-        assertEquals(2, (int) p);
-        p = st.pop();
-        assertNull(p);
+        st1.push (7);
+        p1 = st1.pop();
+        assertEquals(7, p1.intValue());
+
+        s1= st1.size();
+        assertEquals(2, s1);
+        pik1 = st1.peek();
+        assertEquals(pik1.intValue(), 3);
+
+        p1 = st1.pop();
+        assertEquals(3, p1.intValue());
+
+        s1= st1.size();
+        assertEquals(1, s1);
+        pik1 = st1.peek();
+        assertEquals(pik1.intValue(), 2);
+
+        p1 = st1.pop();
+        assertEquals(2, p1.intValue());
+        s1= st1.size();
+        assertEquals(0, s1);
+        pik1 = st1.peek();
+        assertNull(pik1);
+
+        p1 = st1.pop();
+        assertNull(p1);
 
     }
 }
