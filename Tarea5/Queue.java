8764d3af10f1ab7dd222c4e817aa69445c9e939d
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-10-07 20:55:32 +0300

    Add files via upload

---
+++ Tarea5/Queue.java
@@ -0,0 +1,76 @@
+public class Queue<T>
+{
+    public Node <T> head;
+    public Node <T>tail;
+
+    public  Queue() {
+        head = null;
+        tail = null;
+        // init internal storage of stack
+    }
+
+
+    public void enqueue(T item)
+    {   // вставка в хвост
+        Node<T> new_n = new Node<>(item);
+
+        if (this.head == null) {
+            this.head = new_n;
+            this.head.next = null;
+            this.head.prev = null;
+        } else {
+            this.tail.next = new_n;
+            new_n.prev = tail;
+        }
+        this.tail = new_n;
+    }
+
+    public T dequeue()
+    {
+        if (this.head == null) {
+            return null;
+        }
+        T ret =  this.head.value; //
+        this.head = this.head.next;
+
+        if (this.head == null) {
+            this.tail = null;
+        } else {
+            this.head.prev = null;
+        }
+        return ret;
+    }
+
+
+    public int size()
+    {
+        int n = 0;
+
+        Node<T> cur_node = this.head;
+        while (cur_node != null) {
+            n++;
+            cur_node = cur_node.next;
+        }
+        return n;
+    }
+
+
+    public static void main (String[] args) {
+
+    }
+}
+
+
+class Node<T>
+{
+    public T value;
+    public Node<T> next;
+    public Node<T> prev;
+
+    public Node(T _value)
+    {
+        value = _value;
+        next = null;
+        prev = null;
+    }
+}
