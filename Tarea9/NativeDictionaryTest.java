ec81bac302b8b1b17fbce0cd969fcd050d44b885
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-10-30 19:47:19 +0300

    Update NativeDictionaryTest.java

--- Tarea9/NativeDictionaryTest.java
+++ Tarea9/NativeDictionaryTest.java
@@ -1,54 +1,68 @@
-import  org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.lang.reflect.Array;
+import java.util.Objects;
 
-public class NativeDictionaryTest {
-    @Test
-    public void test_1() {
-        NativeDictionary <String> Dict1 = new NativeDictionary<> (17, String.class);
-        int res = Dict1.hashFun("amigo");
-        assertEquals (res, 15);
+class NativeDictionary<T>
+{
+    public int size;
+    public String [] slots;// keys
+    public T [] values;
 
-        res = Dict1.hashFun("a");
-        assertEquals (res, 12);
-        res = Dict1.hashFun("b");
-        assertEquals (res, 13);
-        res = Dict1.hashFun("c");
-        assertEquals (res, 14);
-        res = Dict1.hashFun("d");
-        assertEquals (res, 15);
-
-
-        res = Dict1.hashFun("");
-        assertEquals (res, 0);
-
-        res = Dict1.hashFun("3");
-        assertEquals (res, 0);
-
-        String  ret = Dict1.get("bbvbvb");
-        assertTrue(ret == null);
-
-        Dict1.put("a", "Juan");//
-        Dict1.put("b", "Julia");
-        Dict1.put("c", "Kate");
-        Dict1.put("d", "Alex");
-
-        boolean iskey;
-        assertEquals (Dict1.isKey("a"), true);
-        assertEquals (Dict1.isKey("b"), true);
-        assertEquals (Dict1.isKey("c"), true);
-        assertEquals (Dict1.isKey("d"), true);
-        assertEquals (Dict1.isKey("e"), false);
-
-        assertTrue(Dict1.isKey("amigo") == false);
-
-
-        ret = Dict1.get("a");
-        assertTrue(ret == "Juan");
-        ret = Dict1.get("b");
-        assertTrue(ret == "Julia");
-        ret = Dict1.get("bbvbvb");
-        assertTrue(ret == null);
-
+    public NativeDictionary(int sz, Class clazz)
+    {
+        size = sz;
+        slots = new String[size];
+        values = (T[]) Array.newInstance(clazz, this.size);
     }
+
+    public int hashFun(String key)
+    { // return real index of slot
+        if (key.equals(""))
+            return 0;
+
+        int ind = 0;
+        for ( int i = 0; i< key.length(); i++) {
+            ind = ind + (int)(key.charAt(i));
+        }
+        ind = ind % this.size;
+        return ind;
+    }
+
+    public int rindex (String key){   //real index for same hash & dif key
+       int first_index = this.hashFun(key);
+       int i = first_index;
+        while (this.slots[i] != null && this.slots[i] != key) {
+            i = (i + 1) % this.size;
+            if (i == first_index) // stop cicle
+                return -1;
+        }
+        return i;
+    }
+
+
+    public boolean isKey(String key)
+    {                                // return true if thereis the key int the Dict
+        int ind = this.rindex(key);  // int value of the String key-element of Slots
+
+        if (ind >= 0 && Objects.equals(slots[ind], key)) {
+            return true;
+        }
+            return false;
+    }
+
+    public void put(String key, T value)
+    {  // Add value to key
+        int index = this.rindex(key);
+        this.slots[index] = key;
+        this.values[index] =  value;
+    }
+
+    public T get(String key)
+    {// return value for key or null
+        int index = this.rindex(key);
+        if (index >= 0) {
+            return (this.values[index]);
+        }
+        return null;
+    }
+
 }
