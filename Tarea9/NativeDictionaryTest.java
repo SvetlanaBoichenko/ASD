c1231175515408bab954481920525b69afb620ac
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-10-30 15:55:21 +0300

    Update NativeDictionaryTest.java

--- Tarea9/NativeDictionaryTest.java
+++ Tarea9/NativeDictionaryTest.java
@@ -5,29 +5,50 @@ import static org.junit.Assert.assertTrue;
 public class NativeDictionaryTest {
     @Test
     public void test_1() {
-         NativeDictionary <String> Dict1 = new NativeDictionary<> (17, String.class);
-         int res = Dict1.hashFun("amigo");
-         assertEquals (res, 15);
+        NativeDictionary <String> Dict1 = new NativeDictionary<> (17, String.class);
+        int res = Dict1.hashFun("amigo");
+        assertEquals (res, 15);
 
-         res = Dict1.hashFun("a");
-         assertEquals (res, 12);
+        res = Dict1.hashFun("a");
+        assertEquals (res, 12);
+        res = Dict1.hashFun("b");
+        assertEquals (res, 13);
+        res = Dict1.hashFun("c");
+        assertEquals (res, 14);
+        res = Dict1.hashFun("d");
+        assertEquals (res, 15);
 
-         res = Dict1.hashFun("");
-         assertEquals (res, 0);
 
-         res = Dict1.hashFun("3");
-         assertEquals (res, 0);
+        res = Dict1.hashFun("");
+        assertEquals (res, 0);
 
-         Dict1.put("amigo", "Juan");
-         Dict1.put("a", "Benicasim");
+        res = Dict1.hashFun("3");
+        assertEquals (res, 0);
 
-         assertTrue(Dict1.isKey("amigo") == true);
-         assertTrue(Dict1.isKey("amigos") == false);
+        String  ret = Dict1.get("bbvbvb");
+        assertTrue(ret == null);
 
-         String ret = Dict1.get("a");
-         assertTrue(ret == "Benicasim");
-         ret = Dict1.get("b");
-         assertTrue(ret == null);
+        Dict1.put("a", "Juan");//
+        Dict1.put("b", "Julia");
+        Dict1.put("c", "Kate");
+        Dict1.put("d", "Alex");
+
+        boolean iskey;
+        assertEquals (Dict1.isKey("a"), true);
+        assertEquals (Dict1.isKey("b"), true);
+        assertEquals (Dict1.isKey("c"), true);
+        assertEquals (Dict1.isKey("d"), true);
+        assertEquals (Dict1.isKey("e"), false);
+
+        assertTrue(Dict1.isKey("amigo") == false);
+
+
+        ret = Dict1.get("a");
+        assertTrue(ret == "Juan");
+        ret = Dict1.get("b");
+        assertTrue(ret == "Julia");
+        ret = Dict1.get("bbvbvb");
+        assertTrue(ret == null);
 
     }
 }
