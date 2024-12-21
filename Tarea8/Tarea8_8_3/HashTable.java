7486688bb8aace583c867f914efe190cdbdb8e9f
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-12-21 10:10:33 +0300

    Update HashTable.java

--- Tarea8/Tarea8_8_3/HashTable.java
+++ Tarea8/Tarea8_8_3/HashTable.java
@@ -64,10 +64,22 @@ public class HashTable
     }
 
 //---------------зад 5 dd-----------------------
-    public void dd (){
-        for (int i = 0; i < this.size*2; i++) {
-            this.put(String.valueOf(i));
-        }
+   public void dd (){
+        for (int i = 0; i < this.size*600; i+= 3) {
+            if (i%2 != 0){
+                this.put (String.valueOf(i));
+                this.put (String.valueOf(i+1));
+                this.put (String.valueOf(i+2));
+                this.put (String.valueOf(i+3));
+                this.put (String.valueOf(i+4));
+            }
+            else
+                this.put (String.valueOf(i*300));
+                this.put (String.valueOf(i*2));
+
+                this.put (String.valueOf((i+1)*300));
+                this.put (String.valueOf((i+1)*2));
+            }
     }
 
 //----------Hash + Sal----------
