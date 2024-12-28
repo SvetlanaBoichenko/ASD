277e1cf14677b0392332a4b686567937b7fb9faf
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-12-29 02:19:22 +0300

    Update NativeDictionary2nTest.java

--- Tarea9/Tarea9_2_2/NativeDictionary2nTest.java
+++ Tarea9/Tarea9_2_2/NativeDictionary2nTest.java
@@ -6,58 +6,60 @@ public class NativeDictionary2nTest {
     @Test
     public void test_1() {
         NativeDictionary2n <String> Dict1 = new NativeDictionary2n<> (String.class);
-        int res = Dict1.hashFun("a");
+        int res = Dict1.hashFun(1);
         assertEquals (res, 1);
 
 
-        res = Dict1.hashFun("b");
-        assertEquals (res, 2);
-        res = Dict1.hashFun("c");
+        res = Dict1.hashFun(35);
         assertEquals (res, 3);
-        res = Dict1.hashFun("d");
-        assertEquals (res, 4);
+        res = Dict1.hashFun(10);
+        assertEquals (res, 10);
+        res = Dict1.hashFun(40);
+        assertEquals (res, 8);
 
 
-        res = Dict1.hashFun("");
-        assertEquals (res, 0);
+        res = Dict1.hashFun(null);
+        assertEquals (res, -1);
 
-          res = Dict1.hashFun("3");
+        res = Dict1.hashFun(51);
         assertEquals (res, 19);
 
-        String  ret = Dict1.get("bbvbvb");
+        String  ret = Dict1.get(15);
         assertTrue(ret == null);
 
-        Dict1.put("a", "Juan");//
-        Dict1.put("b", "Julia");
-        Dict1.put("c", "Kate");
-        Dict1.put("d", "Alex");
-        Dict1.put("V", "Alt");
+        Dict1.put(1, "Juan");//
+        Dict1.put(33, "Julia");
+        Dict1.put(3, "Kate");
+        Dict1.put(35, "Alex");
+        Dict1.put(5, "Alt");
 
-        int find = Dict1.rindex("j");
+        int find = Dict1.rindex(10);
         assertTrue(find == 10);
-        find = Dict1.rindex("V");
-        assertTrue(find == 22);
+        find = Dict1.rindex(53);
+        assertTrue(find == 21);
 
-        assertEquals (Dict1.isKey("a"), true);
-        assertEquals (Dict1.isKey("b"), true);
-        assertEquals (Dict1.isKey("c"), true);
-        assertEquals (Dict1.isKey("d"), true);
-        assertEquals (Dict1.isKey("e"), false);
+        assertEquals (Dict1.isKey(1), true);
+        assertEquals (Dict1.isKey(33), true);
+        assertEquals (Dict1.isKey(3), true);
+        assertEquals (Dict1.isKey(35), true);
+        assertEquals (Dict1.isKey(5), true);
 
-        assertTrue(Dict1.isKey("amigo") == false);
+        assertTrue(Dict1.isKey(16) == false);
 
 
-        ret = Dict1.get("a");
+        ret = Dict1.get(1);
         assertTrue(ret == "Juan");
-        ret = Dict1.get("b");
+        ret = Dict1.get(33);
         assertTrue(ret == "Julia");
-        ret = Dict1.get("bbvbvb");
+        ret = Dict1.get(19);
         assertTrue(ret == null);
 
-        ret = Dict1.get("g");
+        ret = Dict1.get(20);
         assertTrue(ret == null);
 
 
     }
 }
+
+
 
