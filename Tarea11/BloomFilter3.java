a7978300219cb682a66819c3458fd80e82a25940
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-01-09 14:39:03 +0300

    Add files via upload

---
+++ Tarea11/BloomFilter3.java
@@ -0,0 +1,80 @@
+// Array with counters (to have posible to delete)
+public class BloomFilter3
+{
+    public int filter_len;
+
+    int [] counters;//
+    public BloomFilter3(int f_len)
+    {
+        counters = new int[f_len];
+        filter_len = f_len;
+    }
+
+    public int hash1(String str1)
+    {// 17
+        int res = 0;
+        for (int i = 0; i < str1.length(); i++)
+        {
+            int code = (int)str1.charAt(i);
+            res = (17 * res + code) % this.filter_len;
+        }
+        return res;
+    }
+    public int hash2(String str1)
+    {  // 223
+        int res = 0;
+        for (int i = 0; i < str1.length(); i++)
+        {
+            int code = (int)str1.charAt(i);
+            res = (223 * res + code) % this.filter_len;
+        }
+        return res;
+    }
+
+    public int getmask (String str1){
+        int h1 = this.hash1(str1);
+        int h2 = this.hash2(str1);
+        return (1 << h1 | 1<< h2);
+    }
+
+    public void add(String str1)
+    {   // add str1 to filter
+        int mask = getmask (str1);
+        for (int i=0; i<this.filter_len; i++)
+        {
+            this.counters[i] += mask % 2;
+            mask = mask >> 1;
+        }
+    }
+
+    public void remove(String str1)
+    {   //
+        if (isValue(str1,1))
+        {
+            int mask = getmask (str1);
+            for (int i=0; i<this.filter_len; i++)
+            {
+                this.counters[i] -= mask % 2;
+                mask = mask >> 1;
+            }
+        }
+    }
+
+    public boolean isValue(String str1, int Q)
+    {
+        int mask = getmask (str1);
+        for (int i=0; i<this.filter_len; i++)
+        {
+            if ((mask % 2 == 1) & (this.counters[i] < Q))
+            {
+                return false;
+            }
+            mask = mask >> 1;
+        }
+        return true;
+    }
+
+public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
